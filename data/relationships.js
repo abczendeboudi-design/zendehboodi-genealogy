bbdad1ce2c44637ebbfdd921d98386c760ed7698
abczendeboudi-design <abczendeboudi@gmail.com>
2026-09-15 08:09:15 +0330

    Create relationships.js

---
+++ data/relationships.js
@@ -0,0 +1,24 @@
+window.RELATIONSHIPS = [
+
+    // همسر سرشاخه
+    {
+        type: "spouse",
+        person1: "person-1",
+        person2: "person-2"
+    },
+
+    // فرزند اول
+    {
+        type: "parent",
+        parent: "person-1",
+        child: "person-3"
+    },
+
+    // فرزند دوم
+    {
+        type: "parent",
+        parent: "person-1",
+        child: "person-4"
+    }
+
+];
