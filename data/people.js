2629da3965fcb15aa5601a74d0780dde21e9bbe6
abczendeboudi-design <abczendeboudi@gmail.com>
2026-09-15 08:07:54 +0330

    Create people.js

---
+++ data/people.js
@@ -0,0 +1,51 @@
+window.PEOPLE = {
+
+    "person-1": {
+        name: "سرشاخه خاندان زنده‌بودی‌ها",
+        gender: "male",
+        occupation: "",
+        education: "",
+        birthDate: "",
+        deathDate: "",
+        residence: "",
+        notes: "",
+        image: ""
+    },
+
+    "person-2": {
+        name: "همسر",
+        gender: "female",
+        occupation: "",
+        education: "",
+        birthDate: "",
+        deathDate: "",
+        residence: "",
+        notes: "",
+        image: ""
+    },
+
+    "person-3": {
+        name: "فرزند اول",
+        gender: "male",
+        occupation: "",
+        education: "",
+        birthDate: "",
+        deathDate: "",
+        residence: "",
+        notes: "",
+        image: ""
+    },
+
+    "person-4": {
+        name: "فرزند دوم",
+        gender: "female",
+        occupation: "",
+        education: "",
+        birthDate: "",
+        deathDate: "",
+        residence: "",
+        notes: "",
+        image: ""
+    }
+
+};
